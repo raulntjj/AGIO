1e1a5832be1ef4beb4c12eff83485d2d44c4e055
raulntjj <raul.oliveira@versatecnlogia.com.br>
2024-12-02 17:51:36 -0300

    finalizando consumo a API externa

--- src/controllers/apiIntegrationController.js
+++ src/controllers/apiIntegrationController.js
@@ -3,16 +3,16 @@ import * as apiIntegrationService from '../services/apiIntegrationService.js';
 export const getExternalApiData = async (request, response) => {
   try {
     const data = await apiIntegrationService.fetchExternalApiData();
-    response.status(200).json({
-		statusCode: 200,
-		message : 'Dados estáticos mockados recuperados com sucesso!',
-	 	 payload : data
+    response.status(200).send({
+      statusCode: 200,
+      message: 'Dados recuperados com sucesso!',
+      payload: data,
     });
-	} catch (error) {
-		const statusCode = error.statusCode || 500;
-		return response.status(statusCode).send({
-			statusCode: statusCode,
-			message: error.message || 'Erro interno do servidor.',
-		});
-	}
+  } catch (error) {
+    const statusCode = error.response?.status || 500;
+    response.status(statusCode).send({
+      statusCode,
+      message: error.message || 'Erro interno do servidor.',
+    });
+  }
 };
--- src/services/apiIntegrationService.js
+++ src/services/apiIntegrationService.js
@@ -1,20 +1,16 @@
-import fetch from 'node-fetch';
-
+import axios from 'axios';
 
 export const fetchExternalApiData = async () => {
-	try {
-		// Fazer a requisição GET para a API
-		const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  try {
+    // Faz a requisição GET para a API
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
 
-		// Verificar se o status da resposta é 2xx
-		if (!response.ok) {
-			throw new Error(`Erro ao buscar dados: Status ${response.status}`);
-		}
-
-		// Retornar os dados convertidos para JSON
-		return await response.json();
-	} catch (error) {
-		// Lidar com erros (lançar para o chamador tratar)
-		throw new Error(`Erro na requisição: ${error.message}`);
-	}
+    // Retorna os dados no campo `data`
+    return response.data;
+  } catch (error) {
+    // Lança um erro com a mensagem apropriada e o status se disponível
+    throw new Error(
+      `Erro na requisição: ${error.response?.status} - ${error.message}`
+    );
+  }
 };
